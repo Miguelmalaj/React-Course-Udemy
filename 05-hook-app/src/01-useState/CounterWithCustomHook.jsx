e03468e167b9fe6c5bec601646e87999885c00e0
Miguelmlj <hsmiguelhs@gmail.com>
2023-02-26 15:49:49 -0700

    git deleted from inside folders

--- 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
+++ 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -1,14 +1,18 @@
+import { useCounter } from "../hooks/useCounter"
 
 export const CounterWithCustomHook = () => {
+
+    const { counter, increment, decrement, reset } = useCounter();
+
   return (
     <>
 
-        <h1>Counter with Hook: ZZZ</h1>
+        <h1>Counter with Hook: { counter }</h1>
         <hr />
 
-        <button className="btn btn-primary">+1</button>
-        <button className="btn btn-primary">Reset</button>
-        <button className="btn btn-primary">-1</button>
+        <button className="btn btn-primary" onClick={() => increment(2)} >+1</button>
+        <button className="btn btn-primary" onClick={reset}>Reset</button>
+        <button className="btn btn-primary" onClick={() => decrement(2)}>-1</button>
 
     </>
   )
